b360503c083b1113ccb75dff7bc585465a92f5f9
Alexandru David Dumitru <alex.dumitru.g@gmail.com>
2025-10-28 09:51:07 +0200

    debugged; added small functionality for master volume

---
+++ functionalities.js
@@ -0,0 +1,10 @@
+// MASTER VOLUME OUTPUT
+const rangeInput = document.getElementById('master-volume');
+const rangeOutput = document.getElementById('rangeValue');
+
+rangeOutput.textContent = rangeInput.value;
+
+rangeInput.addEventListener('input', function () {
+    rangeOutput.textContent = this.value;
+});
+
